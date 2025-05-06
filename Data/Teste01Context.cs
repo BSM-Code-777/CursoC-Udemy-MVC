bda6ca61c39242f3a687c9ee07f9163129d7100d
BSM-Code-777 <bruno.sm.code@hotmail.com>
2025-05-06 12:51:35 -0300

    CRUD Scaffolding

---
+++ Data/Teste01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Teste01.Models;
+
+namespace Teste01.Data
+{
+    public class Teste01Context : DbContext
+    {
+        public Teste01Context (DbContextOptions<Teste01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teste01.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Teste01.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Teste01Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Teste01Context") ?? throw new InvalidOperationException("Connection string 'Teste01Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
